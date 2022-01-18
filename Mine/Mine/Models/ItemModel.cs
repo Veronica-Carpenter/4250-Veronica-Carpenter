c2caea902dfb2207cb35131e5b1e0153053f8295
Veronica-Carpenter <veronica1north@gmail.com>
2022-01-18 15:36:55 -0800

    Added Comments to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -4,8 +4,13 @@ namespace Mine.Models
 {
     public class ItemModel
     {
+        // The Id for the Item
         public string Id { get; set; }
+
+        // The Display Text for the Item
         public string Text { get; set; }
+
+        // The Description for the Item
         public string Description { get; set; }
 
         // The Value of the Item +9 Damage
