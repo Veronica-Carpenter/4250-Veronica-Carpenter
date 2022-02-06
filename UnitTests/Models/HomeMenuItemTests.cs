90604cc7d5af677b16f436663a7d6f9a9155e6e4
Veronica-Carpenter <veronica1north@gmail.com>
2022-02-06 09:10:56 -0800

    Added UT for HomeMenuItem to HomeMenuItemTests.cs

---
+++ UnitTests/Models/HomeMenuItemTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Mine.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class HomeMenuItemTests
+    {
+        [Test]
+        public void HomeMenuItem_Constructor_Valid_()
+        {
+            // Arrange
+
+            // Act
+            var result = new HomeMenuItem();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void HomeMenuItem_Set_Get_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new HomeMenuItem();
+            result.Id = MenuItemType.Items;
+            result.Title = "Title";
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(MenuItemType.Items, result.Id);
+            Assert.AreEqual("Title", result.Title);
+        }
+    }
+}
