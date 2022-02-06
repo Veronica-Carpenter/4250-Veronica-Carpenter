527379c977543ea631be3cc2373ef1a08faa0db5
Veronica-Carpenter <veronica1north@gmail.com>
2022-02-06 08:39:47 -0800

    Added ItemModelTests.cs to the project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_()
+        {
+            // Arrange
+
+            // Act
+
+            // Reset
+
+            // Assert
+            Assert.Pass();
+        }
+    }
+}
