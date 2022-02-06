e7caf81e7cebc86b9a8e166003c8aa7978df6584
Veronica-Carpenter <veronica1north@gmail.com>
2022-02-06 09:26:18 -0800

    Added DiceHelperTests.cs to the project

---
+++ UnitTests/Helpers/DiceHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.Helpers;
+using Mine.Models;
+using NUnit.Framework;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    public class DiceHelperTests
+    {
+        [Test]
+        public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+
+            // Reset
+
+            // Assert
+            Assert.Pass();
+        }
+    }
+}
